7cdb2e0ba623a4276621605571e603db67f4e8bc
Diego Urrego <diegourrg@gmail.com>
2022-05-19 15:03:35 -0500

    Unit testing for the component GifGrid finished.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
-  const { data:images, loading } = useFetchGifs(category);
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3 className='animate__animated animate__fadeInRight'>{category}</h3>
-      {loading && <p className='animate__animated animate__flash'>Loading...</p>}
+      {loading && (
+        <p className='animate__animated animate__flash'>Loading...</p>
+      )}
       <div className='card-grid'>
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />
@@ -16,6 +19,10 @@ const GifGrid = ({ category }) => {
       </div>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
 
 export default GifGrid;
--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { shallow } from 'enzyme/build';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
 import GifGrid from '../../components/GifGrid';
 
+jest.mock('../../hooks/useFetchGifs');
+
 describe('Pruebas en GifGrid', () => {
+  const category = 'Dragon Ball';
+
   test('Debe de mostrar el componente correctamente', () => {
-    const category = 'Goku';
+    // ! Este es EL valor inicial que tiene nuestro estado en useFetchGifs
+    useFetchGifs.mockReturnValue({
+      data: [],
+      loading: true,
+    });
     const wrapper = shallow(<GifGrid category={category} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Debe de mostrar items cuando se caragan imágenes useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'ABC',
+        url: 'https://localhost/cualquier/cosa.jpg',
+        title: 'Cualquier Cosa',
+      },
+      {
+        id: 'ABCD',
+        url: 'https://localhost/cualquier/otraCosa.jpg',
+        title: 'Cualquier Otra Cosa',
+      },
+    ];
+    // ! Al traer "información" nuestro estado debe cambiar, tal cual como lo hace nuestro componente
+    useFetchGifs.mockReturnValue({
+      data: gifs,
+      loading: false,
+    });
+    const wrapper = shallow(<GifGrid category={category} />);
+    // expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('p').exists()).toBe(false);
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
   });
 });
